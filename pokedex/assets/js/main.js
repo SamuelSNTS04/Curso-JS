730aaaedc8595cd867414608aa2ed69917de0363
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-21 14:35:40 -0300

    Código de manipulação de HTML gerando um novo HTML com a estrutura map

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,9 +1,33 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-//processamento assíncrono - não tem a resposta de imediato
-fetch(url).then(function (response) {
-    console.log(response);
+function convertPokemonToLi(pokemon) {
+    return `<li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png"
+                        alt="${pokemon.name}">
+                </div>
+            </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 })
+
+// esse código funciona da mesma maneira que o map
+/* pokeApi.getPokemons().then((pokemons = []) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon);
+    }
+})
+.catch((error) => console.error(error)) */
 
