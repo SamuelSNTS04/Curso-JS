ade355b7a1ae02e78b1ce12ea78444a9d2d74636
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-21 15:16:49 -0300

    Trazendo os detalhes de cada pokemon na requisição

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,16 +1,19 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `<li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
                     </ol>
 
-                    <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png"
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
                         alt="${pokemon.name}">
                 </div>
             </li>`
