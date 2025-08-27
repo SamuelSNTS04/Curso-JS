9b338fe7bb033766113997d38067fde298ee6da5
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-27 00:00:43 -0300

    Segunda parte da aula - função construtora

--- aulas/aula07.js
+++ aulas/aula07.js
@@ -1,13 +1,23 @@
-//__proto__
+//função construtora
 
-const pessoa = {
-    genero: "masculino"
+function Pesssoa(nome, idade) {
+    this.nome = nome
+    this.idade = idade
 }
 
-const samuel = {
-    nome: 'samuel',
-    idade: 21,
-    __proto__: pessoa
+Pesssoa.prototype.falar = function () {
+    console.log(`Meu nome é: ${this.nome}`)
 }
 
-console.log(samuel.genero)
+//o código abaixo opera da mesma forma, só está mais padronizado com outras linguagens
+
+class Pessoa {
+    constructor(nome, idade) {
+        this.nome = nome
+        this.idade = idade
+    }
+
+    falar() {
+        console.log(`Meu nome é: ${this.nome}`)
+    }
+}
