f22c8a98e53dfc0289ed35c9c2366fd34210e633
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-28 16:45:22 -0300

    funcao map

--- aulas/aula08.js
+++ aulas/aula08.js
@@ -1,11 +1,17 @@
-// filter
+// map
 
-const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+class Pessoa{
+    constructor(name) {
+        this.name = name;
+    }
+}
 
-//filter recebe uma função que retorna um boolean, se for true entra na nova lista se for false não entra
-//filter retorna uma nova lista com o que foi filtrado
-const listaDeNumerosPares = lista.filter((element) => {
-    return (element % 2 === 0);
-})
+const lista = [new Pessoa('samuel'), new Pessoa('tiago'), new Pessoa('bianca'), new Pessoa('ezequiel')];
 
-console.log(listaDeNumerosPares);
+//conversão da lista pessoa, para a lista de nomes - conversão de um objeto para outro
+const listaNomes = lista.map((element) => {
+    return element.name;
+});
+
+console.log(listaNomes);
+
