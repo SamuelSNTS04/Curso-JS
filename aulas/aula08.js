5b28846eb86cf1e1042b8d238f6743474faf1ee1
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-28 12:27:03 -0300

    manipulação de lista com filter

--- aulas/aula08.js
+++ aulas/aula08.js
@@ -1,14 +1,11 @@
-// foreach
+// filter
 
 const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-/*
-lista.forEach((value, i, listRef) => {
-    console.log(value, i, listRef);
-});
-*/
+//filter recebe uma função que retorna um boolean, se for true entra na nova lista se for false não entra
+//filter retorna uma nova lista com o que foi filtrado
+const listaDeNumerosPares = lista.filter((element) => {
+    return (element % 2 === 0);
+})
 
-lista.forEach((value, i, listRef) => {
-    console.log(value + i);
-});
-
+console.log(listaDeNumerosPares);
