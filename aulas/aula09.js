a742335de6dddf5a61c24a6c4fb91111c9dfc1b9
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-29 16:37:47 -0300

    Async e Await

--- aulas/aula09.js
+++ aulas/aula09.js
@@ -1,20 +1,15 @@
 //assincronismo
 
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
 const filePath = path.resolve(__dirname, 'tarefas.csv');
 
-const promessaDaLeituraDoArquivo = fs.promises.readFile("tarefas.csv");
+async function buscarArquivo() {
+    const arquivo = await fs.promises.readFile(filePath);
+    const textoDoArquivo = arquivo.toString('utf-8');
 
-promessaDaLeituraDoArquivo
-    .then((arquivo) => arquivo.toString("utf-8"))
-    .then((textoDoArquivo) => textoDoArquivo.split('\n').slice(1))
-    .then((linhasSemCabecalho) => linhasSemCabecalho.map((linha) => {
-        const [nome, feito] = linha.split(';');
-        return {
-            nome,
-            feito: feito.trim() === "true"
-        }
-    }))
-    .then((listaDasTarefas) => console.log(listaDasTarefas))
-    .catch((error) => console.log("deu ruim", error))
+    console.log(textoDoArquivo);
+}
+
+buscarArquivo();
