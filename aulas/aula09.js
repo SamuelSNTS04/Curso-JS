4a4864ea86db28dbec5d88877a7601e8ea6aaa8d
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-08-29 16:01:03 -0300

    Simulando uma chamada assíncrona

---
+++ aulas/aula09.js
@@ -0,0 +1,16 @@
+//assincronismo
+
+const promessaDeUmNumeroQualquer = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const numero = parseInt(Math.random() * 100);
+        resolve(numero);
+    }, 1000);
+});
+
+promessaDeUmNumeroQualquer.then((value) => {
+    console.log(value);
+}).catch((error) => {
+    console.error(error);
+}).finally(() => {
+    console.log("finalizou!");
+})
